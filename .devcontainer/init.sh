f6a9f0b515a4da621476f37b74e930d951815bea
isksss <104404522+isksss@users.noreply.github.com>
2023-08-07 10:39:58 +0000

    systemd

--- .devcontainer/init.sh
+++ .devcontainer/init.sh
@@ -11,5 +11,9 @@ sudo apt install -y \
 # システムユーザ
 sudo useradd -U -s /sbin/nologin -r minecraft
 
-sudo ln -sf ../.settings/cron/cron_server /etc/cron.d/cron_server
+sudo ln -sf /mc/.settings.d/cron/cron_server /etc/cron.d/cron_server
+sudo ln -sf /mc/.settings.d/systemd/start_mc_server.service /etc/systemd/start_mc_server.service
+
 sudo service cron restart
+sudo systemctl enable start_mc_server.service
+sudo systemctl restart auto_shell.service
